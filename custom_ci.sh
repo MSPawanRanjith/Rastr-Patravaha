9bc565aa26d66ae47fef32b1e1aa806af71799a8
MSPawanRanjith <pawanranjith@gmail.com>
2020-05-10 08:45:38 +0530

    ssh keygen trial

---
+++ custom_ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo $(pwd)
+echo $HOME
+echo setting git config...
+git config --global user.name "rashtr" 
+git config --global user.email "pawanranjith@gmail.com" 
+echo $(git config --global --list)
+
+ssh-keygen -t rsa -b 4096 -C "pawanranjith@gmail.com" -N '' -f ~/.ssh/id_rsa
+echo trying cat to show id_rsa
+cat ~/.ssh/id_rsa
+
+echo trying normal to show id_rsa
+<~/.ssh/id_rsa
+
+
+gatsby build --prefix-paths && gh-pages -d public -b gh-pages -r https://github.com/MSPawanRanjith/Rashtr-Patravaha.git
