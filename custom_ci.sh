222ebc568435fea59a16a4ad0812c76dff825759
MSPawanRanjith <pawanranjith@gmail.com>
2020-05-10 09:21:47 +0530

    added deploy key in script

--- custom_ci.sh
+++ custom_ci.sh
@@ -7,15 +7,15 @@ git config --global user.name "rashtr"
 git config --global user.email "pawanranjith@gmail.com" 
 echo $(git config --global --list)
 
-ssh-keygen -t rsa -b 4096 -C "pawanranjith@gmail.com" -N '' -f ~/.ssh/id_rsa
-echo trying cat to show id_rsa
-cat ~/.ssh/id_rsa
+# ssh-keygen -t rsa -b 4096 -C "pawanranjith@gmail.com" -N '' -f ~/.ssh/id_rsa
+# echo trying cat to show id_rsa
+# cat ~/.ssh/id_rsa
 
-echo trying cat to show .ssh/id_rsa.pub.
-cat ~/.ssh/id_rsa.pub
+# echo trying cat to show .ssh/id_rsa.pub.
+# cat ~/.ssh/id_rsa.pub
 
-echo trying normal to show id_rsa
-<~/.ssh/id_rsa
+# echo trying normal to show id_rsa
+# <~/.ssh/id_rsa
 
 
-gatsby build --prefix-paths && gh-pages -d public -b gh-pages -r https://github.com/MSPawanRanjith/Rashtr-Patravaha.git
+gatsby build --prefix-paths && gh-pages -d public -b gh-pages -r git@github.com:MSPawanRanjith/Rashtr-Patravaha.git
